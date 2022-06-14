9707893d51f46ef57227ec2f618683251d8d21af
dbwowns10 <jeffyoo10@naver.com>
2022-06-14 16:02:54 +0900

    jaejoon

--- study/01_study/jaejoon/back.py
+++ study/01_study/jaejoon/back.py
@@ -1,45 +1,36 @@
 #https://deep-learning-study.tistory.com/272
 
 import cv2
-import sys
 
+#웹캠 설정
 capture = cv2.VideoCapture(0)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # 배경 영상 등록
 ret, back = capture.read()
-
-if not ret:
-    print('Background image registration failed!')
-    sys.exit()
     
 # 연산 속도를 높이기 위해 그레이스케일 영상으로 변환
 back = cv2.cvtColor(back, cv2.COLOR_BGR2GRAY)
 
-# 가우시안 블러로 노이즈 제거 (모폴로지, 열기, 닫기 연산도 가능)
-back = cv2.GaussianBlur(back, (0, 0), 1.0)
-
 # 비디오 매 프레임 처리
 while True:
-    ogret, ogframe = capture.read()
     ret, frame = capture.read()
-    
-    if not ret:
-        break
     
     # 현재 프레임 영상 그레이스케일 변환
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # 노이즈 제거
-    gray = cv2.GaussianBlur(gray, (0, 0), 1.0)
     
-    # 차영상 구하기 $ 이진화
     # absdiff는 차 영상에 절대값
     diff = cv2.absdiff(gray, back)
-    # 차이가 30이상 255(흰색), 30보다 작으면 0(검정색)
+
+    # 차이가 30이상 1, 40보다 작으면 0 -> 필터 역할
     _, diff = cv2.threshold(diff, 40, 1, cv2.THRESH_BINARY)
-    diff2 = cv2.merge((diff, diff, diff))
-    cv2.imshow('back', diff2 * ogframe)
+
+    #필터를 3채널로 만듦
+    diff3 = cv2.merge((diff, diff, diff))
+
+    #원래 프레임에 필터를 곱해서 전경 부분만 추출
+    cv2.imshow('back', diff3 * frame)
 
     if cv2.waitKey(33) > 0:
         break
