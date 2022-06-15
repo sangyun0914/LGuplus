f28ca84235b247d12b7acbc7114271cfa7ddea9b
EastShineK <ehdqls0212@naver.com>
2022-06-15 10:39:25 +0900

    add yolov4

---
+++ study/01_study/pre-study_Sangyun/README.md
@@ -0,0 +1,24 @@
+# Created environment for running yolov4 with tensorflow
+Enables GPU acceleration for silicon Apple chip
+Modify conda-gpu.yml for other GPU model usage
+
+# Creating environment
+conda env create -f conda-gpu.yml
+
+conda activate yolov4-gpu
+
+conda install -c apple tensorflow-deps     
+conda install -c apple tensorflow-deps==2.6.0    
+python -m pip install tensorflow-macos
+python -m pip install tensorflow-metal
+
+conda install easydict -c conda-forge 
+python convert_trt.py --weights ./checkpoints/yolov4.tf --quantize_mode float16 --output ./checkpoints/yolov4-trt-fp16-416
+
+
+# Running cam detection model
+python detect_video.py --weights ./checkpoints/yolov4-416 --size 416 --model yolov4 --video 0 --output ./detections/results.avi
+
+
+# Result: Does not perform as well in terms of FPS 
+Average 7.0 FPS in M1 Pro Metal
--- study/01_study/pre-study_dongbeen/dev.py
+++ study/01_study/pre-study_dongbeen/dev.py
@@ -1,6 +1,23 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+
+#yolo v4
+CONFIDENCE_THRESHOLD = 0.2
+NMS_THRESHOLD = 0.4
+COLORS = [(0,255,255), (255,255,0), (0,255,0), (255,0,0)]
+
+class_name = []
+with open("classes.txt", "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+
+net = cv2.dnn.readNet("yolov4.weights", "yolov4.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(416,416), scale=1/255, swapRB=True)
 
 cap = cv2.VideoCapture(0)
 while True:
@@ -53,6 +70,21 @@ while True:
         small = cv2.resize(src[y: y + h, x: x + w], None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
         src[y: y + h, x: x + w] = cv2.resize(small, (w, h), interpolation=cv2.INTER_NEAREST)
 
+    #yolov4
+    start = time.time()
+    classes, scores, boxes = model.detect(cam, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    end = time.time()
+
+    start_drawing = time.time()
+    for (classid, score, box) in zip(classes, scores, boxes):
+        color = COLORS[int(classid) % len(COLORS)]
+        label = "%s : %f" % (class_names[classid], score)
+        cv2.rectangle(cam, box, color, 2)
+        cv2.putText(cam, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    end_drawing = time.time()
+
+    fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (1/(end-start), (end_drawing - start_drawing) * 1000)
+    cv2.putText(cam, fps_label, (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),2)
 
     #detectImage = cv2.cvtColor(detectImage, cv2.COLOR_GRAY2BGR)
 
