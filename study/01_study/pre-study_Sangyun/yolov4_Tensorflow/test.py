9b242e10fda4bab053c07ff5c93ab906d88e1388
신상윤 <sangyun@Sangyuns-MacBook-Pro.local>
2022-06-15 13:49:10 +0900

    Yolov4 GPU Acceleration

---
+++ study/01_study/pre-study_Sangyun/yolov4_Tensorflow/test.py
@@ -0,0 +1,13 @@
+# test_environment.py
+    
+import sys
+    
+import tensorflow.keras
+import tensorflow as tf
+    
+print(f"Tensor Flow Version: {tf.__version__}")
+print(f"Keras Version: {tensorflow.keras.__version__}")
+print()
+print(f"Python {sys.version}")
+gpu = len(tf.config.list_physical_devices('GPU'))>0
+print("GPU is", "available" if gpu else "NOT AVAILABLE")
