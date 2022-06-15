1a85617169c83ad86cec0c168f0b999de43df8ac
dbwowns10 <jeffyoo10@naver.com>
2022-06-15 16:08:41 +0900

    jaejoon

--- study/01_study/jaejoon/main.py
+++ study/01_study/jaejoon/main.py
@@ -31,11 +31,10 @@ np.random.seed(42)
 colors = np.random.randint(0, 255, size=(len(classes), 3), dtype='uint8')
 
 while(True):
-    ret, frame = capture.read()
-    height, width, channels = frame.shape
+    ret, frame1 = capture.read()
 
     # 현재 프레임 영상 그레이스케일 변환
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     
     # 배경 영상과 현재 프레임의 차이 절댓값 구함
     diff = cv2.absdiff(gray, back1)
@@ -51,29 +50,31 @@ while(True):
     filter2 = cv2.merge((filter1, filter1, filter1))
 
     # 전경 추출, 흑백
-    back2 = mask.apply(frame)
+    back2 = mask.apply(frame1)
     back2 = cv2.cvtColor(back2, cv2.COLOR_GRAY2BGR)
 
     # 원본 영상에서 전경 추출을 위해 필터 생성
     _, filter3 = cv2.threshold(back2, 30, 1, cv2.THRESH_BINARY)
 
     # 영상 여러개 한번에 보여주기 위해 합치는 과정
-    numpy_horizontal = np.hstack((frame * filter2, frame * filter3))
+    numpy_horizontal = np.hstack((frame1 * filter2, frame1 * filter3))
 
     # 합친 화면 띄워줌
     #cv2.imshow('background', numpy_horizontal)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # 흑백으로 변환
+    ret, frame2 = capture.read()
+
+    gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY) # 흑백으로 변환
 
     # threshold    
     ret, th = cv2.threshold(gray ,127, 255, cv2.THRESH_BINARY) # 임계값(127) 넘으면 백, 아니면 흑으로 바꿈
     th2 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2) # 이미지 영역별로 이진화 진행, 영역의 평균값을 임계값으로 설정, 영역(블록)사이즈 = 15
 
     # blur
-    blur = cv2.blur(frame, (5, 5)) # 5 * 5 박스내 픽셀 평균값
-    gblur = cv2.GaussianBlur(frame, (15,15), 0) # 2차원 가우시안 분포 이용
-    mblur = cv2.medianBlur(frame, 7) # 박스 내 픽셀들의 중간값, 박스 사이즈 = 7 
-    bfblur = cv2.bilateralFilter(frame, 10, 75, 75) # 윤곽선은 살리고 나머지는 블러 처리
+    blur = cv2.blur(frame2, (5, 5)) # 5 * 5 박스내 픽셀 평균값
+    gblur = cv2.GaussianBlur(frame2, (15,15), 0) # 2차원 가우시안 분포 이용
+    mblur = cv2.medianBlur(frame2, 7) # 박스 내 픽셀들의 중간값, 박스 사이즈 = 7 
+    bfblur = cv2.bilateralFilter(frame2, 10, 75, 75) # 윤곽선은 살리고 나머지는 블러 처리
 
     # canny edge detection
     canny = cv2.Canny(gray, 70, 200)
@@ -99,8 +100,11 @@ while(True):
     #cv2.imshow('threshold, canny edge', result1)
     #cv2.imshow('blur, mosaic', result2)
 
+    ret, frame3 = capture.read()
+    height, width, channels = frame3.shape
+
     # YOLO
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(frame3, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
 
@@ -133,13 +137,13 @@ while(True):
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
             color = [int(c) for c in colors[class_ids[i]]]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.rectangle(frame3, (x, y), (x + w, y + h), color, 2)
             text = "{}: {:.4f}".format(classes[class_ids[i]], confidences[i])
-            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+            cv2.putText(frame3, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
 
 
     result3 = np.hstack((gblur, bfblur, mosaic))
-    result4 = np.hstack((frame * filter2, frame * filter3, frame))
+    result4 = np.hstack((frame1 * filter2, frame1 * filter3, frame3))
     result5 = np.vstack((result1, result3, result4))
 
 
