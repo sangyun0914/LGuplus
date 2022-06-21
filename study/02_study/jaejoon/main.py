cfcef9f460657cd361efaab2c3b33e202095fa1f
dbwowns10 <jeffyoo10@naver.com>
2022-06-21 10:14:41 +0900

    jj

--- study/02_study/jaejoon/main.py
+++ study/02_study/jaejoon/main.py
@@ -4,11 +4,11 @@
 
 
 import cv2
-import labeling as mylabel
+# import labeling as mylabel
 import numpy as np
-import sys
+# import sys
 
-sys.setrecursionlimit(10**7)
+# sys.setrecursionlimit(10**7)
 
 video = cv2.VideoCapture('test2.mp4')
 mask = cv2.bgsegm.createBackgroundSubtractorMOG()
@@ -19,10 +19,18 @@ while(True):
     # print(frame.shape)
     # (576, 768, 3)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # blur = cv2.dilate(gray, kernel=None)
     blur = cv2.GaussianBlur(gray, (15, 15), 0)
     back = mask.apply(blur)
 
-    print(len(mylabel.labeling(back)))
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(back)
+    print(ret - 1)
+
+    for s in stats:
+        cv2.rectangle(frame, (s[0], s[1]),
+                      (s[0] + s[2], s[1]+s[3]), (255, 0, 0), 3)
+
+    # print(len(mylabel.labeling(back)))
 
     result = np.hstack((frame, cv2.cvtColor(back, cv2.COLOR_GRAY2BGR)))
     cv2.imshow('back', result)
