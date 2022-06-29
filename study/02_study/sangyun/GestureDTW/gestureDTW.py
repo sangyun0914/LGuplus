dcd5b1de7ac2533ee862e19d5808e4c2f77cc446
신상윤 <sangyun0914@g.skku.edu>
2022-06-29 13:17:27 +0900

    Add gesture recognition DTW

---
+++ study/02_study/sangyun/GestureDTW/gestureDTW.py
@@ -0,0 +1,413 @@
+import cv2
+import mediapipe as mp
+import os
+from tqdm import tqdm
+import pandas as pd
+import pickle as pkl
+import numpy as np
+from fastdtw import fastdtw
+from collections import Counter
+from models.sign_model import SignModel
+
+
+WHITE_COLOR = (245, 242, 226)
+RED_COLOR = (25, 35, 240)
+
+HEIGHT = 600
+
+def load_dataset():
+    videos = [
+        file_name.replace(".mp4", "")
+        for root, dirs, files in os.walk(os.path.join("data", "videos"))
+        for file_name in files
+        if file_name.endswith(".mp4")
+    ]
+    dataset = [
+        file_name.replace(".pickle", "").replace("pose_", "")
+        for root, dirs, files in os.walk(os.path.join("data", "dataset"))
+        for file_name in files
+        if file_name.endswith(".pickle") and file_name.startswith("pose_")
+    ]
+
+    # Create the dataset from the reference videos
+    videos_not_in_dataset = list(set(videos).difference(set(dataset)))
+    n = len(videos_not_in_dataset)
+    if n > 0:
+        print(f"\nExtracting landmarks from new videos: {n} videos detected\n")
+
+        for idx in tqdm(range(n)):
+            save_landmarks_from_video(videos_not_in_dataset[idx])
+
+    return videos
+
+
+def landmark_to_array(mp_landmark_list):
+    """Return a np array of size (nb_keypoints x 3)"""
+    keypoints = []
+    for landmark in mp_landmark_list.landmark:
+        keypoints.append([landmark.x, landmark.y, landmark.z])
+    return np.nan_to_num(keypoints)
+
+def extract_landmarks(results):
+    """Extract the results of both hands and convert them to a np array of size
+    if a hand doesn't appear, return an array of zeros
+
+    :param results: mediapipe object that contains the 3D position of all keypoints
+    :return: Two np arrays of size (1, 21 * 3) = (1, nb_keypoints * nb_coordinates) corresponding to both hands
+    """
+    pose = landmark_to_array(results.pose_landmarks).reshape(99).tolist()
+
+    left_hand = np.zeros(63).tolist()
+    if results.left_hand_landmarks:
+        left_hand = landmark_to_array(results.left_hand_landmarks).reshape(63).tolist()
+
+    right_hand = np.zeros(63).tolist()
+    if results.right_hand_landmarks:
+        right_hand = (
+            landmark_to_array(results.right_hand_landmarks).reshape(63).tolist()
+        )
+    return pose, left_hand, right_hand
+
+
+def save_landmarks_from_video(video_name):
+    landmark_list = {"pose": [], "left_hand": [], "right_hand": []}
+    sign_name = video_name.split("-")[0]
+
+    # Set the Video stream
+    cap = cv2.VideoCapture(
+        os.path.join("data", "videos", sign_name, video_name + ".mp4")
+    )
+    with mp.solutions.holistic.Holistic(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+
+                # Store results
+                pose, left_hand, right_hand = extract_landmarks(results)
+                landmark_list["pose"].append(pose)
+                landmark_list["left_hand"].append(left_hand)
+                landmark_list["right_hand"].append(right_hand)
+            else:
+                break
+        cap.release()
+
+    # Create the folder of the sign if it doesn't exists
+    path = os.path.join("data", "dataset", sign_name)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    # Create the folder of the video data if it doesn't exists
+    data_path = os.path.join(path, video_name)
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+
+    # Saving the landmark_list in the correct folder
+    save_array(
+        landmark_list["pose"], os.path.join(data_path, f"pose_{video_name}.pickle")
+    )
+    save_array(
+        landmark_list["left_hand"], os.path.join(data_path, f"lh_{video_name}.pickle")
+    )
+    save_array(
+        landmark_list["right_hand"], os.path.join(data_path, f"rh_{video_name}.pickle")
+    )
+
+def save_array(arr, path):
+    file = open(path, "wb")
+    pkl.dump(arr, file)
+    file.close()
+
+def load_array(path):
+    file = open(path, "rb")
+    arr = pkl.load(file)
+    file.close()
+    return np.array(arr)
+
+def load_reference_signs(videos):
+    reference_signs = {"name": [], "sign_model": [], "distance": []}
+    for video_name in videos:
+        sign_name = video_name.split("-")[0]
+        path = os.path.join("data", "dataset", sign_name, video_name)
+
+        left_hand_list = load_array(os.path.join(path, f"lh_{video_name}.pickle"))
+        right_hand_list = load_array(os.path.join(path, f"rh_{video_name}.pickle"))
+
+        reference_signs["name"].append(sign_name)
+        reference_signs["sign_model"].append(SignModel(left_hand_list, right_hand_list))
+        reference_signs["distance"].append(0)
+    
+    reference_signs = pd.DataFrame(reference_signs, dtype=object)
+    print(
+        f'Dictionary count: {reference_signs[["name", "sign_model"]].groupby(["name"]).count()}'
+    )
+    return reference_signs
+
+
+class SignRecorder(object):
+    def __init__(self, reference_signs: pd.DataFrame, seq_len=50):
+        # Variables for recording
+        self.is_recording = False
+        self.seq_len = seq_len
+
+        # List of results stored each frame
+        self.recorded_results = []
+
+        # DataFrame storing the distances between the recorded sign & all the reference signs from the dataset
+        self.reference_signs = reference_signs
+
+    def record(self):
+        """
+        Initialize sign_distances & start recording
+        """
+        self.reference_signs["distance"].values[:] = 0
+        self.is_recording = True
+
+    def process_results(self, results):
+        """
+        If the SignRecorder is in the recording state:
+            it stores the landmarks during seq_len frames and then computes the sign distances
+        :param results: mediapipe output
+        :return: Return the word predicted (blank text if there is no distances)
+                & the recording state
+        """
+        if self.is_recording:
+            if len(self.recorded_results) < self.seq_len:
+                self.recorded_results.append(results)
+            else:
+                self.compute_distances()
+                print(self.reference_signs)
+
+        if np.sum(self.reference_signs["distance"].values) == 0:
+            return "", self.is_recording
+        return self._get_sign_predicted(), self.is_recording
+
+    def compute_distances(self):
+        """
+        Updates the distance column of the reference_signs
+        and resets recording variables
+        """
+        left_hand_list, right_hand_list = [], []
+        for results in self.recorded_results:
+            _, left_hand, right_hand = extract_landmarks(results)
+            left_hand_list.append(left_hand)
+            right_hand_list.append(right_hand)
+
+        # Create a SignModel object with the landmarks gathered during recording
+        recorded_sign = SignModel(left_hand_list, right_hand_list)
+
+        # Compute sign similarity with DTW (ascending order)
+        self.reference_signs = dtw_distances(recorded_sign, self.reference_signs)
+
+        # Reset variables
+        self.recorded_results = []
+        self.is_recording = False
+
+    def _get_sign_predicted(self, batch_size=5, threshold=0.5):
+        """
+        Method that outputs the sign that appears the most in the list of closest
+        reference signs, only if its proportion within the batch is greater than the threshold
+
+        :param batch_size: Size of the batch of reference signs that will be compared to the recorded sign
+        :param threshold: If the proportion of the most represented sign in the batch is greater than threshold,
+                        we output the sign_name
+                          If not,
+                        we output "Sign not found"
+        :return: The name of the predicted sign
+        """
+        # Get the list (of size batch_size) of the most similar reference signs
+        sign_names = self.reference_signs.iloc[:batch_size]["name"].values
+
+        # Count the occurrences of each sign and sort them by descending order
+        sign_counter = Counter(sign_names).most_common()
+
+        predicted_sign, count = sign_counter[0]
+        if count / batch_size < threshold:
+            return "Sign not found"
+        return predicted_sign
+
+
+def dtw_distances(recorded_sign: SignModel, reference_signs: pd.DataFrame):
+    """
+    Use DTW to compute similarity between the recorded sign & the reference signs
+
+    :param recorded_sign: a SignModel object containing the data gathered during record
+    :param reference_signs: pd.DataFrame
+                            columns : name, dtype: str
+                                      sign_model, dtype: SignModel
+                                      distance, dtype: float64
+    :return: Return a sign dictionary sorted by the distances from the recorded sign
+    """
+    # Embeddings of the recorded sign
+    rec_left_hand = recorded_sign.lh_embedding
+    rec_right_hand = recorded_sign.rh_embedding
+
+    for idx, row in reference_signs.iterrows():
+        # Initialize the row variables
+        ref_sign_name, ref_sign_model, _ = row
+
+        # If the reference sign has the same number of hands compute fastdtw
+        if (recorded_sign.has_left_hand == ref_sign_model.has_left_hand) and (
+            recorded_sign.has_right_hand == ref_sign_model.has_right_hand
+        ):
+            ref_left_hand = ref_sign_model.lh_embedding
+            ref_right_hand = ref_sign_model.rh_embedding
+
+            if recorded_sign.has_left_hand:
+                row["distance"] += list(fastdtw(rec_left_hand, ref_left_hand))[0]
+            if recorded_sign.has_right_hand:
+                row["distance"] += list(fastdtw(rec_right_hand, ref_right_hand))[0]
+
+        # If not, distance equals infinity
+        else:
+            row["distance"] = np.inf
+    return reference_signs.sort_values(by=["distance"])
+
+
+class WebcamManager(object):
+    """Object that displays the Webcam output, draws the landmarks detected and
+    outputs the sign prediction
+    """
+
+    def __init__(self):
+        self.sign_detected = ""
+
+    def update(
+        self, frame: np.ndarray, results, sign_detected: str, is_recording: bool
+    ):
+        self.sign_detected = sign_detected
+
+        # Draw landmarks
+        self.draw_landmarks(frame, results)
+
+        WIDTH = int(HEIGHT * len(frame[0]) / len(frame))
+        # Resize frame
+        frame = cv2.resize(frame, (WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+
+        # Flip the image vertically for mirror effect
+        frame = cv2.flip(frame, 1)
+
+        # Write result if there is
+        frame = self.draw_text(frame)
+
+        # Chose circle color
+        color = WHITE_COLOR
+        if is_recording:
+            color = RED_COLOR
+
+        # Update the frame
+        cv2.circle(frame, (30, 30), 20, color, -1)
+        cv2.imshow("OpenCV Feed", frame)
+
+    def draw_text(
+        self,
+        frame,
+        font=cv2.FONT_HERSHEY_COMPLEX,
+        font_size=1,
+        font_thickness=2,
+        offset=int(HEIGHT * 0.02),
+        bg_color=(245, 242, 176, 0.85),
+    ):
+        window_w = int(HEIGHT * len(frame[0]) / len(frame))
+
+        (text_w, text_h), _ = cv2.getTextSize(
+            self.sign_detected, font, font_size, font_thickness
+        )
+
+        text_x, text_y = int((window_w - text_w) / 2), HEIGHT - text_h - offset
+
+        cv2.rectangle(frame, (0, text_y - offset), (window_w, HEIGHT), bg_color, -1)
+        cv2.putText(
+            frame,
+            self.sign_detected,
+            (text_x, text_y + text_h + font_size - 1),
+            font,
+            font_size,
+            (118, 62, 37),
+            font_thickness,
+        )
+        return frame
+
+    @staticmethod
+    def draw_landmarks(image, results):
+        mp_holistic = mp.solutions.holistic  # Holistic model
+        mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+
+        # Draw left hand connections
+        mp_drawing.draw_landmarks(
+            image,
+            landmark_list=results.left_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(232, 254, 255), thickness=1, circle_radius=1
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(255, 249, 161), thickness=2, circle_radius=2
+            ),
+        )
+        # Draw right hand connections
+        mp_drawing.draw_landmarks(
+            image,
+            landmark_list=results.right_hand_landmarks,
+            connections=mp_holistic.HAND_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing.DrawingSpec(
+                color=(232, 254, 255), thickness=1, circle_radius=2
+            ),
+            connection_drawing_spec=mp_drawing.DrawingSpec(
+                color=(255, 249, 161), thickness=2, circle_radius=2
+            ),
+        )
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+
+
+# Create dataset of the videos where landmarks have not been extracted yet
+videos = load_dataset()
+
+# Create a DataFrame of reference signs (name: str, model: SignModel, distance: int)
+reference_signs = load_reference_signs(videos)
+
+# Object that stores mediapipe results and computes sign similarities
+sign_recorder = SignRecorder(reference_signs)
+
+# Object that draws keypoints & displays results
+webcam_manager = WebcamManager()
+
+# Turn on the webcam
+cap = cv2.VideoCapture(0)
+# Set up the Mediapipe environment
+with mp.solutions.holistic.Holistic(
+    min_detection_confidence=0.5, min_tracking_confidence=0.5
+) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+
+        # Process results
+        sign_detected, is_recording = sign_recorder.process_results(results)
+
+        # Update the frame (draw landmarks & display result)
+        webcam_manager.update(frame, results, sign_detected, is_recording)
+
+        pressedKey = cv2.waitKey(1) & 0xFF
+        if pressedKey == ord("r"):  # Record pressing r
+            sign_recorder.record()
+        elif pressedKey == ord("q"):  # Break pressing q
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
