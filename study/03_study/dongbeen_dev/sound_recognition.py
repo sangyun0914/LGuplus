cba1dc46a76768e58ffdffd40c0b57829d88f32e
EastShineK <ehdqls0212@naver.com>
2022-07-08 13:48:52 +0900

    repeat speech recognition

--- study/03_study/dongbeen_dev/sound_recognition.py
+++ study/03_study/dongbeen_dev/sound_recognition.py
@@ -1,25 +1,27 @@
 import speech_recognition as sr
 import sys #-- 텍스트 저장시 사용
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say Something")
-    speech = r.listen(source)
 
-#sys.stdout = open('audio_output.txt', 'w') #-- 텍스트 저장시 사용
+while 1:
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say Something")
+        speech = r.listen(source)
 
-try:
-    audio = r.recognize_google(speech, language="ko-KR")
-    print("Your speech thinks like\n " + audio)
-    if audio == "시작":
-        print("Hello")
-    elif audio == "끝":
-        print("end")
-    else:
-        print("nothing")
-except sr.UnknownValueError:
-    print("Your speech can not understand")
-except sr.RequestError as e:
-    print("Request Error!; {0}".format(e))
+    #sys.stdout = open('audio_output.txt', 'w') #-- 텍스트 저장시 사용
+
+    try:
+        audio = r.recognize_google(speech, language="ko-KR")
+        print("Your speech thinks like\n " + audio)
+        if audio == "시작":
+            print("Hello")
+        elif audio == "종료":
+            print("end")
+        else:
+            print("nothing")
+    except sr.UnknownValueError:
+        print("Your speech can not understand")
+    except sr.RequestError as e:
+        print("Request Error!; {0}".format(e))
 
 #sys.stdout.close() #-- 텍스트 저장시 사용
