09be7848a26d7326cfc05cab09d5a3cd74ad0092
EastShineK <ehdqls0212@naver.com>
2022-07-21 10:28:53 +0900

    edit test code

--- study/04_study/dongbeen/02_test_frame_30_fine.py
+++ study/04_study/dongbeen/02_test_frame_30_fine.py
@@ -10,6 +10,7 @@ from torchvision import transforms
 import mediapipe as mp
 import numpy as np
 import math
+from torchvision import models #[resnet, alexnet, vgg, squeezenet, densenet, inception]
 
 MHI_DURATION = 30
 
@@ -240,14 +241,115 @@ def InsertCoordinate(image,landmark_pose):
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-num_classes = train_data_set.num_classes
-custom_model = CustomConvNet(num_classes=num_classes).to(device)
-custom_model.load_state_dict(torch.load('actionModelv1.pt'))
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+            
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    return model_ft, input_size
+
+# Top level data directory. Here we assume the format of the direc`ory conforms
+#   to the ImageFolder structure
+data_dir = "./data"
+
+# Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
+model_name = "resnet"
+
+# Number of classes in the dataset
+num_classes = 3
+
+# Batch size for training (change depending on how much memory you have)
+batch_size = 32
+
+# Number of epochs to train for
+num_epochs = 10
+
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
+
+# Initialize the model for this run
+model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
+
+#model = models.resnet18() # 기본 가중치를 불러오지 않으므로 pretrained=True를 지정하지 않습니다.
+model_ft.load_state_dict(torch.load('model_weights_v0_2_0.pth'))
+model_ft.eval()
 
 mp_drawing = mp.solutions.drawing_utils
 mp_selfie_segmentation = mp.solutions.selfie_segmentation
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+
+class_names = ['lunge', 'squat', 'stand']
 
 with torch.no_grad():
   cap = cv2.VideoCapture(0)
@@ -283,14 +385,18 @@ with torch.no_grad():
         tensor_img = transform(frame_process)
         tensor_img = tensor_img.unsqueeze(0)
 
-        outputs = custom_model(tensor_img)
+        outputs = model(tensor_img)
         first , predicted = torch.max(outputs.data, 1)
+        print(predicted)
+        print(class_names[predicted])
 
         action = None
         if (predicted == 0):
-          action = "stand"
-        else:
+          action = "lunge"
+        elif (predicted == 1):
           action = "squat"
+        else:
+          action = "stand"
 
         if (first > 0.5):
           print("action : {}, prob : {}".format(action,first))
