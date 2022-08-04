5a4d7c3761a5dc7a4f990dbb7a1709f5496762b9
ajs3801 <tt14an@gmail.com>
2022-08-04 09:39:34 +0900

    implements runtest

--- module/TestMix/EvalAnswer.py
+++ module/TestMix/EvalAnswer.py
@@ -1,7 +1,7 @@
 # TestVideo 파일이름을 input으로 넣으면 올바른 정답 return
 def EvalAnswer(name):
   if ("SLP") in name:
-    return  "SSLLPP"
+    return "SSLLPP"
   elif ("SPL") in name:
     return "SSPPLL"
   elif ("PSL") in name:
--- sub_project/Posture/test.py
+++ sub_project/Posture/test.py
@@ -7,31 +7,4 @@ if __name__ == "__main__":
   cur_path = os.getcwd()
   test_path = Test.ReturnTestPath("TestVideo")
   test_list = Test.ReturnTestList(test_path)
-
-  correct = 0
-  for i in test_list:
-    MODEL = "model/ActionV6_rf.pkl"
-    print("===========================================================")
-    VIDEO_PATH = os.path.join("TestVideo",i)
-    print("CURRENT VIDEO PATH : {}".format(VIDEO_PATH))
-    print("===========================================================")
-    cap = cv2.VideoCapture(VIDEO_PATH)
-    predict = Engine.TestEngine(cap,MODEL)
-    print("Answer  is {}".format(Test.EvalAnswer(i)))
-    print("Predict is {}".format(predict))
-
-    if predict == Test.EvalAnswer(i):
-      correct+=1
-  
-  print("===========================================================")
-  print("Test Accuracy -> {}%".format(correct/len(test_list)*100))
-
-
-  # TESTCAM
-  # cap = cv2.VideoCapture(VIDEO_PATH)
-
-  # REALTIME CAM
-  # cap = cv2.VideoCapture(0)
-  
-  # Engine.StartEngine(cap)
-  # Engine.InferenceEngine(cap,MODEL)
+  Test.RunTest(test_list)
